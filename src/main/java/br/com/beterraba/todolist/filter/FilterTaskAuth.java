8ad53030313c3cbcdfad1029e5f93ee498489c59
Lucas Vieira <ls6182315@gmail.com>
2023-10-11 14:56:57 -0300

    feat: add filter validation

--- src/main/java/br/com/beterraba/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/beterraba/todolist/filter/FilterTaskAuth.java
@@ -1,9 +1,12 @@
 package br.com.beterraba.todolist.filter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.beterraba.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,13 +16,31 @@ import java.util.Base64;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var authorization = request.getHeader("Authorization");
+
         var authEncoded = authorization.substring("Basic".length()).trim();
         byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
         var authString = new String(authDecoded);
+
         String[] credentials = authString.split(":");
-        filterChain.doFilter(request,response);
+
+        var user = this.userRepository.findByUsername(credentials[0]);
+        if (user == null) {
+            response.sendError(401);
+        } else {
+            var isValidPassword = BCrypt.verifyer().verify(credentials[1].toCharArray(),user.getPassword());
+            if (isValidPassword.verified) {
+                filterChain.doFilter(request,response);
+            } else {
+                response.sendError(401);
+            }
+
+        }
+
     }
 }
